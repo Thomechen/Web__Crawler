fffed9d1f1396b0e2f46a24a624d694fda5cf5c7
Austin <island6614@gmail.com>
2020-10-26 14:55:09 +0800

    資料解析

--- oil_price.py
+++ oil_price.py
@@ -7,3 +7,8 @@ Created on Mon Oct 26 14:26:55 2020
 import pandas as pd
 import numpy as np
 r = pd.read_excel("oil_price.xlsx",sheet_mame="oil_price")
+date = r["調價日期"]
+oil_92 = r["92 無鉛汽油"]
+oil_95 = r["95 無鉛汽油"]
+oil_98 = r["98 無鉛汽油"]
+oil_super = r["超級/高級柴油"]
