02f71151c8a605652c970b11cfb8e679a4a99d7f
Austin <island6614@gmail.com>
2020-10-26 14:48:30 +0800

    create

---
+++ oil_price.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 14:26:55 2020
+
+@author: islan
+"""
+import pandas as pd
+import numpy as np
+r = pd.read_excel("oil_price.xlsx",sheet_mame="oil_price")
