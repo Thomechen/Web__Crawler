1ac1ab7ab41c58f0e34692c4baf01ac9064eaba6
Austin <island6614@gmail.com>
2020-11-02 16:56:41 +0800

    Update yahoo_news.py
    1.增加顯示分隔線
    2.增加項目個數寄數

--- yahoo_news.py
+++ yahoo_news.py
@@ -19,11 +19,14 @@ if r.status_code == 200:
     #頭條標題
     links = soup.find_all('a',class_='D(ib) Ov(h) Whs(nw) C($c-fuji-grey-l) C($c-fuji-blue-1-c):h Td(n) Fz(16px) Tov(e) Fw(700)')
     #標題連結
+    count = 1
     for t,l in zip(titles,links) :
-        print(t.text)
+        print('---[',count,']----------------------------------')
+        print('標題:'+t.text)
         title_list.append(t.text)
-        print(l.get('href'))
+        print('網址:'+l.get('href'))
         links_list.append(l.get('href'))
+        count += 1
 data = {'story_title':title_list,'links':links_list}
 df = pd.DataFrame(data)
 #資料框架建立
