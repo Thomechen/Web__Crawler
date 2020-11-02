02bc4c471db7053b920a476324f4b28e4e034694
Austin <island6614@gmail.com>
2020-11-02 15:06:32 +0800

    Update yahoo_news.py
    1.頭條標題擷取
    2.連結擷取
    3.資料框架建立

--- yahoo_news.py
+++ yahoo_news.py
@@ -8,13 +8,25 @@ Created on Mon Nov  2 13:54:04 2020
 import requests
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
 request = requests.Session()
 url = 'https://tw.news.yahoo.com/'
-categroy = ['entertainment']
 r = request.get(url)
+title_list = []
+#
+links_list = []
 if r.status_code == 200:
     r = request.get(url)
     soup = BeautifulSoup(r.text,'html.parser')
     titles = soup.find_all('li',class_='Pos(r) Lh(1.5) H(24px) Mb(8px)')
-    for t in titles :
+    #頭條標題
+    links = soup.find_all('a',class_='D(ib) Ov(h) Whs(nw) C($c-fuji-grey-l) C($c-fuji-blue-1-c):h Td(n) Fz(16px) Tov(e) Fw(700)')
+    #標題連結
+    for t,l in zip(titles,links) :
         print(t.text)
+        title_list.append(t.text)
+        print(l.get('href'))
+        links_list.append(l.get('href'))
+data = {'story_title':title_list,'links':links_list}
+df = pd.DataFrame(data)
+#資料框架建立
