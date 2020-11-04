7451f994074f2617a52bdb61e7c51fae3328577b
Austin <island6614@gmail.com>
2020-11-04 13:56:17 +0800

    Update Stock_Q.py

--- Stock_Q.py
+++ Stock_Q.py
@@ -16,6 +16,10 @@ year = ['2017','2018','2019']
 
 url = 'http://www.stockq.org/stock/history/'
 
+date_info = []
+
+value_info = []
+
 for y in range(len(year)):
     url_1 = url + year[y] +"/"
     for m in range(1,13,1):
@@ -32,20 +36,28 @@ for y in range(len(year)):
                     url_3 = url_2 + year[y] + str(m) +str(d) +'_tc.php'
                     r = request.get(url_3)
                     soup = BeautifulSoup(r.text,'html.parser')
-                    value = soup.select('tr.row2')[3]
-                    print(value.text)
-                    print(year[y] + str(m) +str(d))
+                    value = soup.select('tr.row2')[3].text
+                    space = "\n"
+                    value_title = value.split(space)[1]
+                    value_value = value.split(space)[2]
+                    date_i =  year[y] + str(m) +str(d)
+                    date_info.append(date_i)
+                    value_info.append(value_value)
                 else:
                     d = str(d)
                     url_3 = url_2 + year[y] + str(m) + str(d) +'_tc.php'
                     r = request.get(url_3)
                     soup = BeautifulSoup(r.text,'html.parser')
-                    value = soup.select('tr.row2')[3]
-                    print(value.text)
-                    print(year[y] + str(m) +str(d))
+                    value = soup.select('tr.row2')[3].text
+                    space = "\n"
+                    value_title = value.split(space)[1]
+                    value_value = value.split(space)[2]
+                    date_i =  year[y] + str(m) +str(d)
+                    date_info.append(date_i)
+                    value_info.append(value_value)
             except:
                 continue
     
-# soup = BeautifulSoup(r.text,'html.parser')
-# value = soup.select('tr.row2')[3]
-# print(value.text)
+data = {'Date':date_info,'Value':value_info}
+df = pd.DataFrame(data,index=date_info)
+print('Done')
