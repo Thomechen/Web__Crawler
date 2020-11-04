e405798c4082541659a599d7fcbaf6dc9f3fb65b
Austin <island6614@gmail.com>
2020-11-04 10:53:52 +0800

    Create Ptt_Gossiping.py

---
+++ Ptt_Gossiping.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+session = requests.Session()
+
+my_data = {"from": "/bbs/Gossiping/index.html",
+           "yes": "yes"}
+
+r = session.post('https://www.ptt.cc/ask/over18',data=my_data)
+
+url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+
+
+while True:
+    r2 = session.get(url)
+    if r2.status_code == 200:
+        soup = BeautifulSoup(r2.text,'html.parser')
+        titles = soup.select('div.title')
+        links = soup.select('div.title > a')
+        date = soup.select('div.date')
+        newUrl = 'https://www.ptt.cc'+soup.select('div.btn-group.btn-group-paging > a')[1].get('href')
+        for t,l,d in zip(titles,links,date):
+            if 'Re:' in t.text or '11/03' not in d.text:
+                 continue
+            print(d.text+' 標題:'+t.text.strip())
+            r3 = session.get('https://www.ptt.cc'+l.get('href'))
+            soup2 = BeautifulSoup(r3.text,'html.parser')
+            content = soup2.select_one('div#main-content').text
+            target_content = u'※ 發信站: 批踢踢實業坊(ptt.cc),'
+            content = content.split(target_content)[0]
+            
+            date = soup2.select('span.article-meta-value')[3].text
+            content = content.split(date)[1]
+            # print(content)
+        url = newUrl
